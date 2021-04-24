94f4cd36a856562eadfebd92fe06a8bc30b8b9ad
vitalii <tregubovvakharkov@gmail.com>
2021-04-24 20:51:27 +0300

    view engine - ejs

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.render('index', { name: 'Vitali', todos: [] });
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about');
 });
 
 app.get('/about-us', (req, res) => {
-  res.redirect('/about');
-})
+  res.redirect('about');
+});
 
 app.use((req, res) => {
-  res.send('<h1>404 Page not found...</h1>')
+  res.render('404');
 });
+
