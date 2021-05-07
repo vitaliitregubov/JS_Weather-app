bff3d06adfce09d9bb0ba224b014f4906552cb99
vitalii <tregubovvakharkov@gmail.com>
2021-05-07 14:04:57 +0300

    basic setup

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+require('dotenv').config();
+
 const app = express();
 
-const productsRoute = require('./routes/products');
-const peopleRouter = require('./routes/people');
-const apiRouter = require('./routes/api');
-
-app.use(express.urlencoded({ extended: false }));
-app.use('/people', peopleRouter);
-app.use('/api', apiRouter);
-app.use('/products', productsRoute);
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile('./index.html', { root: __dirname });
-})
+  res.render('index', { title: 'Home', user: 'Vitalii', tasks: ['learn Node.js', 'speak English', 'exercises', 'cooking', 'buy groceries'] });
+});
 
-app.post('/login', (req, res) => {
-  console.log(req.body);
-})
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+
+app.listen(port);
 
