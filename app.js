5db1f2d089ffe8703c6bf696301a1f36369d5086
vitalii <tregubovvakharkov@gmail.com>
2021-05-10 23:11:14 +0300

    cocntrollers

--- app.js
+++ app.js
@@ -15,24 +15,5 @@ app.use(expressLayouts);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use(userRouter);
-
-const pool = mysql.createPool({
-  connectionLimit: 100,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME 
-});
-
-pool.getConnection((error, connection) => {
-  if(error) {
-    console.log(error);
-  }
-  console.log('connected to a DB');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
-});
+app.use('/users', userRouter);
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,9 @@ const userRouter = require('express').Router();
 const userController = require('../controllers/userController');
 
 userRouter.get('/', userController.view);
+userRouter.post('/', userController.find);
+userRouter.post('/add', userController.form);
+userRouter.get('/:id', userController.viewSingleUser);
 
 module.exports = userRouter;
 
