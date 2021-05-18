197277b668bb3bc43f57abe2de024ca25893c65e
vitalii <tregubovvakharkov@gmail.com>
2021-05-18 13:30:17 +0300

    create user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -27,5 +27,42 @@ exports.view = (req, res) => {
   });
 };
 
+exports.find = (req, res) => {
+  const { search } = req.body;
+  connection.query("SELECT * FROM users WHERE name LIKE ?", ['%' + search + '%'], (error, results) => {
+    if(error) {
+      console.log(error);
+      return;
+    } else {
+      res.render('index', { users: results });
+    }
+  });
+};
 
+exports.userDetails = (req, res) => {
+  connection.query("SELECT * FROM users WHERE id = 2", (error, results) => {
+    if(error) {
+      console.log(error);
+      return;
+    } else {
+      res.render('user', { user: results[0] });
+    }
+  });
+};
+
+exports.form = (req, res) => {
+  res.render('addUser', { alert: '' });
+}
+
+exports.addUser = (req, res) => {
+  const { name, email, phone } = req.body;
+  connection.query("INSERT INTO users(name, email, birthDate) VALUES (?, ?, ?)", [name, email, phone], error => {
+    if(error) {
+      console.log(error);
+      return;
+    } else {
+      res.render('addUser', { alert: 'Created successfully' });
+    }
+  });
+}
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,10 @@ const routes = require('express').Router();
 const userController = require('../controllers/userController');
 
 routes.get('/', userController.view);
+routes.get('/add-user', userController.form);
+routes.post('/add-user', userController.addUser)
+routes.get('/:id', userController.userDetails);
+routes.post('/find', userController.find);
 
 module.exports = routes;
 
